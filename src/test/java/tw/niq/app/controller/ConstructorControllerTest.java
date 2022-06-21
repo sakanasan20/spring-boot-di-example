9d6aa1df5c38dd5afe64da019ebc87ddda5097dc
nick.chen <sakanasan20@gmail.com>
2022-06-21 15:17:29 +0800

    Modify controller test to use different service

--- src/test/java/tw/niq/app/controller/ConstructorControllerTest.java
+++ src/test/java/tw/niq/app/controller/ConstructorControllerTest.java
@@ -3,7 +3,7 @@ package tw.niq.app.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import tw.niq.app.service.GreetingServiceImpl;
+import tw.niq.app.service.GreetingConstructorService;
 
 class ConstructorControllerTest {
 	
@@ -11,7 +11,7 @@ class ConstructorControllerTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		constructorController = new ConstructorController(new GreetingServiceImpl());
+		constructorController = new ConstructorController(new GreetingConstructorService());
 	}
 
 	@Test

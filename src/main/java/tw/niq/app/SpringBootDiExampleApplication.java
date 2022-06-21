3fc281b093159860c7efa7d39060aa9b2fedec21
nick.chen <sakanasan20@gmail.com>
2022-06-21 14:10:53 +0800

    Add application context example

--- src/main/java/tw/niq/app/SpringBootDiExampleApplication.java
+++ src/main/java/tw/niq/app/SpringBootDiExampleApplication.java
@@ -2,12 +2,23 @@ package tw.niq.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import tw.niq.app.controller.HelloController;
 
 @SpringBootApplication
 public class SpringBootDiExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDiExampleApplication.class, args);
+		
+		ApplicationContext ctx = SpringApplication.run(SpringBootDiExampleApplication.class, args);
+		
+		HelloController helloController = (HelloController) ctx.getBean("helloController");
+		
+		String returnedMessage = helloController.sayHello();
+		
+		System.out.println(returnedMessage);
+		
 	}
 
 }

ef9f1cf68298e5134f01703e193a9f7c69b8daa7
nick.chen <sakanasan20@gmail.com>
2022-06-21 15:23:59 +0800

    Switch hello controller to use greeting service

--- src/main/java/tw/niq/app/controller/HelloController.java
+++ src/main/java/tw/niq/app/controller/HelloController.java
@@ -2,14 +2,19 @@ package tw.niq.app.controller;
 
 import org.springframework.stereotype.Controller;
 
+import tw.niq.app.service.GreetingService;
+
 @Controller
 public class HelloController {
+	
+	private final GreetingService greetingService;
+
+	public HelloController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
 
 	public String sayHello() {
-		
-		System.out.println("Hello world!");
-		
-		return "sayHello()";
+		return greetingService.sayGreeting();
 	}
 	
 }

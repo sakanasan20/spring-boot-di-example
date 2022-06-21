ce4adafe148fed6273df884f55d3a29a7131c2e0
nick.chen <sakanasan20@gmail.com>
2022-06-21 15:17:42 +0800

    Add @Qualifier annotation

--- src/main/java/tw/niq/app/controller/PropertyController.java
+++ src/main/java/tw/niq/app/controller/PropertyController.java
@@ -1,6 +1,7 @@
 package tw.niq.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import tw.niq.app.service.GreetingService;
@@ -8,6 +9,7 @@ import tw.niq.app.service.GreetingService;
 @Controller
 public class PropertyController {
 	
+	@Qualifier("greetingPropertyService")
 	@Autowired
 	public GreetingService greetingService;
 

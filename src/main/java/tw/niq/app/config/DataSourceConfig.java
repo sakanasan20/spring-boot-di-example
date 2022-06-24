92af2a3e2239407bea3d1225ebdce967553844b6
nick.chen <sakanasan20@gmail.com>
2022-06-24 08:32:51 +0800

    Create properties binding example

--- src/main/java/tw/niq/app/config/DataSourceConfig.java
+++ src/main/java/tw/niq/app/config/DataSourceConfig.java
@@ -1,24 +1,72 @@
 package tw.niq.app.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import tw.niq.app.datasource.FakeDataSource;
 
+@ConfigurationProperties("tw.niq")
 @Configuration
 public class DataSourceConfig {
+	
+	private String username;
+	private String password;
+	private String jdbcUrl;
+	private String mode;
+	private String driver;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getJdbcUrl() {
+		return jdbcUrl;
+	}
+
+	public void setJdbcUrl(String jdbcUrl) {
+		this.jdbcUrl = jdbcUrl;
+	}
+
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
 
 	@Bean
-	FakeDataSource fakeDataSource(@Value("${tw.niq.username}") String username, 
-			@Value("${tw.niq.password}") String password, 
-			@Value("${tw.niq.jdbc-url}") String jdbcUrl, 
-			@Value("${tw.niq.mode}") String mode) {
+	FakeDataSource fakeDataSource() {
+		
 		FakeDataSource fakeDataSource = new FakeDataSource();
+		
 		fakeDataSource.setUsername(username);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setJdbcUrl(jdbcUrl);
 		fakeDataSource.setMode(mode);
+		fakeDataSource.setDriver(driver);
+		
 		return fakeDataSource;
 	}
 	
